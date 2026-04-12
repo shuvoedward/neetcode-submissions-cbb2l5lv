f93a129bb8c7307e3201f119c8f05e0bf06d5a04
Cornelius Edward <38678545+shuvoedward@users.noreply.github.com>
2026-04-12 16:10:38 -0400

    Add: merge-sorted-array - submission-0

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-0.go
@@ -0,0 +1,15 @@
+func merge(nums1 []int, m int, nums2 []int, n int) {
+last := m + n - 1
+	i, j := m-1, n-1
+
+	for j >= 0 {
+		if i >= 0 && nums1[i] > nums2[j] {
+			nums1[last] = nums1[i]
+			i--
+		} else {
+			nums1[last] = nums2[j]
+			j--
+		}
+		last--
+	}
+}
