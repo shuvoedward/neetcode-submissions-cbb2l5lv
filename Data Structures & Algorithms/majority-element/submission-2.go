5a0e3d9d936277770f85d4f1f6ff6e93ef0eb821
Cornelius Edward <38678545+shuvoedward@users.noreply.github.com>
2026-04-21 16:33:37 -0400

    Add: majority-element - submission-2

---
+++ Data Structures & Algorithms/majority-element/submission-2.go
@@ -0,0 +1,17 @@
+func majorityElement(nums []int) int {
+   candidate, count := 0, 0
+
+   for _, num := range nums{
+	if count == 0{
+		candidate = num
+	}
+
+	if candidate == num{
+		count++
+	}else{
+		count--
+	}
+   }
+
+   return candidate
+}
