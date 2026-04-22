068b118c89fca50f6ce4a3043b85d429bc79fea7
Cornelius Edward <38678545+shuvoedward@users.noreply.github.com>
2026-04-22 17:40:30 -0400

    Add: merge-strings-alternately - submission-3

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-3.go
@@ -0,0 +1,19 @@
+func mergeAlternately(word1 string, word2 string) string {
+	l := len(word1)
+	
+	if len(word2) < l{
+		l = len(word2)
+	}
+
+	var result strings.Builder
+	result.Grow(len(word1) + len(word2))
+	for i := range l{
+		result.WriteByte(word1[i])
+		result.WriteByte(word2[i])
+	}
+
+	result.WriteString(word1[l:])
+	result.WriteString(word2[l:])
+
+	return result.String()
+}
