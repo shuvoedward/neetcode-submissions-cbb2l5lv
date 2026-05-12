6620ce8f8c402de3f2f046eb1161e17efd56863f
Cornelius Edward <38678545+shuvoedward@users.noreply.github.com>
2026-05-12 17:07:31 -0400

    Add: same-binary-tree - submission-1

---
+++ Data Structures & Algorithms/same-binary-tree/submission-1.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    // dfs recursive
+	// if node exists, then value
+	if p == nil && q == nil{
+		return true
+	}
+	
+	if p != nil && q != nil && p.Val == q.Val {
+		return isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+	}
+
+	return false
+}
