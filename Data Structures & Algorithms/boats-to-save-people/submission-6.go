669b86807a7b1e8b72b4263e01213c5de52c2809
Cornelius Edward <38678545+shuvoedward@users.noreply.github.com>
2026-05-18 17:35:48 -0400

    Add: boats-to-save-people - submission-6

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-6.go
@@ -0,0 +1,26 @@
+import "slices"
+func numRescueBoats(people []int, limit int) int {
+	slices.Sort(people)
+
+	// 1, 2, 4, 5
+	l , r := 0, len(people)-1
+	res := 0
+	for l <= r{
+		remainder := limit - people[r]
+		res++
+		r--
+		if l <= r && people[l] <= remainder{
+			l++
+		}
+	}
+
+	return res
+
+}
+
+// for l <= r{
+// 	if people[l] + people[r] > limit{
+// r--}
+// else l++ r--
+// res++
+// }
