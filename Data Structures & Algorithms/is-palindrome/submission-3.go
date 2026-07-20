9f1bd4ac0ba148f13286a70338a28b98b7e810c1
Cornelius Edward <38678545+shuvoedward@users.noreply.github.com>
2026-07-20 13:39:33 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.go
@@ -0,0 +1,23 @@
+func isPalindrome(s string) bool {
+	l, r := 0, len(s)-1
+	srune := []rune(s)
+
+	for l < r {
+		for l < r && !isAlpha(srune[l])  {
+			l++
+		}
+		for l < r && !isAlpha(srune[r]) {
+			r--
+		}
+		if unicode.ToLower(srune[l]) != unicode.ToLower(srune[r]) {
+			return false
+		}
+		l++
+		r--
+	}
+
+	return true
+}
+func isAlpha(r rune) bool {
+	return unicode.IsDigit(r) || unicode.IsLetter(r) 
+}
