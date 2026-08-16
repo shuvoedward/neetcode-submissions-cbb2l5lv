12faf503665ce587fb85edc64fe164ede7920f35
Cornelius Edward <38678545+shuvoedward@users.noreply.github.com>
2026-08-16 19:43:02 -0400

    Add: maximum-sum-circular-subarray - submission-0

---
+++ Data Structures & Algorithms/maximum-sum-circular-subarray/submission-0.go
@@ -0,0 +1,17 @@
+func maxSubarraySumCircular(nums []int) int {
+	globMax, globMin := nums[0], nums[0]
+	curMax, curMin, total := 0, 0, 0
+
+	for _, num := range nums{
+		curMax = max(curMax + num, num)
+		curMin = min(curMin + num, num)
+		total += num
+		globMax = max(globMax, curMax)
+		globMin= min(globMin, curMin)
+	}	
+
+	if globMax > 0{
+		return max(globMax, total-globMin)
+	}
+	return globMax
+}
